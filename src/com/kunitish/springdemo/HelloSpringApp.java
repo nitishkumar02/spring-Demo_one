c930bf9b6a206aac245ba64ec40977cb82b039b6
nk10008 <nkgupta3541@gmail.com>
2020-09-01 14:39:28 +0530

    First Spring App using Config file

---
+++ src/com/kunitish/springdemo/HelloSpringApp.java
@@ -0,0 +1,27 @@
+package com.kunitish.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloSpringApp {
+
+	public static void main(String[] args) {
+		/*
+		 *  1.load the spring config file
+		 *  2.retrive the bean from container
+		 *  3.call methos on bean
+		 *  4.close the context
+		 */
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+		
+		System.out.println(theCoach.getDailyWorkout());
+		
+		context.close();
+		
+		
+		
+
+	}
+
+}
